b5fc2691d240e699a5adbfc5443d297b68bcf5ab
Dmitry <dima.klimov.98@inbox.ru>
2022-04-14 16:47:41 +0300

    task2

---
+++ Task2/src/Function.java
@@ -0,0 +1,21 @@
+public class Function {
+    public static double calculateValue(int n) {
+        if (n > 1) {
+            int mul = 0;
+            for (int i = 1; i <= n; i++) {
+                mul += getFactorial(i);
+            }
+            return ((double) 1 / getFactorial(n)) * mul;
+        } else {
+            System.out.println("N less then 1");
+            return 0;
+        }
+    }
+
+    private static long getFactorial(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        return n * getFactorial(n - 1);
+    }
+}
---
+++ Task2/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 2; i < 20; i++) {
+            System.out.println(Function.calculateValue(i));
+        }
+    }
+}
